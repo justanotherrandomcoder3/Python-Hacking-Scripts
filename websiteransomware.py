0955a2e109f19c75c197ae77bd84a5b0a07a9280
justanotherrandomcoder3 <125522863+justanotherrandomcoder3@users.noreply.github.com>
2023-09-29 14:26:21 -0500

    Add files via upload

---
+++ websiteransomware.py
@@ -0,0 +1,22 @@
+#import all packages
+#Python 3 code
+
+
+
+import os
+import socket as s
+import requests as r
+import subprocess as sub
+
+
+
+while True:
+    os.C:/Passwords
+    sitehacked = client(www.google.com)
+ function(client(sitehacked))
+    filemal = __file__: malware.bat
+s.send(client.filemal(sitehacked))
+req = r.ransom(sitehacked)
+message = ¨Give me bitcoin if you want your site back.¨
+s.send(client(sitehacked)message)
+client.close
