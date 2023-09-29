f2bf35aadf191c8b4aa6c1f54e33bbc25d93dbb5
justanotherrandomcoder3 <125522863+justanotherrandomcoder3@users.noreply.github.com>
2023-09-29 11:05:46 -0500

    Add files via upload

---
+++ ipscanner.py
@@ -0,0 +1,23 @@
+#import all packages
+#Python 3 code
+
+
+import requests as r
+import os
+import subprocess
+import socket as s
+
+
+
+
+
+
+while True:
+req = r.get(https://10.10.100.100)
+ip = s.gethostname(req)
+showiprange = 1
+
+
+while showiprange <9000
+          print(ip)
+          print(ip)
---
+++ simpleipfinder.py
@@ -0,0 +1,19 @@
+#import all packages
+#Python 3 code 
+
+
+import socket as s
+
+
+
+
+
+
+while True:
+          s.gethostname 
+          function getipsimplecode(s.gethostbyname())
+               ip = s.gethostbyname(#paste the link to someones google drive to find their ip)
+               spamipfinder  = 1
+while smapipfinder < 9 
+         print(ip)
+         print(ip)
