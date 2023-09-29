f0f2c311f955d5a00d008094b6c6d4a471077183
justanotherrandomcoder3 <125522863+justanotherrandomcoder3@users.noreply.github.com>
2023-09-29 15:56:40 -0500

    Add files via upload

---
+++ ransomware.py
@@ -0,0 +1,26 @@
+#import all packages
+#Python 3 code 
+
+
+
+
+import os
+import socket as s
+import subprocess as subp
+import requests as r
+
+
+
+
+
+os.dowload(os.C//FILEPATH//USER//FILE//)
+message = ¨Send me bitcoin or your computer will be destroyed.¨ 
+s.send.os.C//USER//(message)
+useranswer = input(¨ Enter the code to your hacked computer.¨)
+if  useranswer = ugothackedlol
+     print(¨Access granted.¨)
+     os.delete(os.C//FILEPATH//USER//FILE//)
+not useranswer = ugothackedlol
+         os.run(os.C//FILEPATH//USER//FILE//)
+print (¨Your computer is gone forever.¨)
+
