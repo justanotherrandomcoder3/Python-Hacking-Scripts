4cbee3b12dd71f0967688bc3909478709b15cfcf
justanotherrandomcoder3 <125522863+justanotherrandomcoder3@users.noreply.github.com>
2023-10-02 18:00:40 -0500

    Create packetresponder.py

---
+++ packetresponder.py
@@ -0,0 +1,13 @@
+import socket as sc
+import random 
+
+host = *(64.233.160.0)
+port = *(122)
+
+while True:
+    sock = sc.sc*(host, port)
+packet = random._urandom*(1024)
+s.send*(sock) *[packet]
+warningfromMM341307 = *("I will destroy Google.")
+s.send*(sock) *[warningfromMM341307]
+s.recv(sock)
