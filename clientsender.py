032f9225219dac937eef2ed2ed0901385394aa78
justanotherrandomcoder3 <125522863+justanotherrandomcoder3@users.noreply.github.com>
2023-10-02 18:42:31 -0500

    Create clientsender.py

---
+++ clientsender.py
@@ -0,0 +1,13 @@
+#connected with clientreciever.py
+import socket as sc
+clientport = 80
+clientip = *(2600:387:a:7::a0)
+
+if 9 < 123541
+    clientserver = sc.sc*(sc.AF_INET(clientip), clientport)
+filepack = f.read*(C:\Users\mm341307@adline-isd.org\Files\Google Drive\testmalwarefourth.bat) #imports a file into the "filepack" variable.
+s.send*(clientserver) *[filepack]
+#|DO NOT COPY THIS FILE PATH|
+s.recv(clientserver)
+
+
