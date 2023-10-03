a6a6421b6ea2da86ce9dfa355806db1772cd63f5
justanotherrandomcoder3 <125522863+justanotherrandomcoder3@users.noreply.github.com>
2023-10-02 19:32:42 -0500

    Update clientsender.py

--- clientsender.py
+++ clientsender.py
@@ -4,7 +4,7 @@ clientport = 80
 clientip = *(2600:387:a:7::a0)
 
 if 9 < 123541
-    clientserver = sc.sc*(sc.AF_INET(clientip), clientport)
+    clientserver = sc.sc*(clientip, clientport)
 filepack = f.read*(C:\Users\mm341307@adline-isd.org\Files\Google Drive\testmalwarefourth.bat) #imports a file into the "filepack" variable.
 sc.send*(clientserver) *[filepack]
 #|DO NOT COPY THIS FILE PATH|
