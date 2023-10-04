6c2a2d434a0276c7aa831c63d2458fa0ba758cf3
justanotherrandomcoder3 <125522863+justanotherrandomcoder3@users.noreply.github.com>
2023-10-04 13:21:42 -0500

    Create ippacketsender.py

---
+++ ippacketsender.py
@@ -0,0 +1,12 @@
+import socket as sc
+import random
+
+
+
+
+while True:
+    clientinfo = sc.sc*(sc.AF_INET*(www.contacts.google.com/u/1/as327764@aldine-isd.org), 80)
+cookiepacket = random._urandom(1024)
+sc.send*(clientinfo) *[cookiepacket]
+sc.recv(clientinfo)
+print(clientinfo)
